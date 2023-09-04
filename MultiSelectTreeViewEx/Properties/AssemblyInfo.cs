b85904b674c1448eddd8e6fff19edb945780bd23
Maximilian Burkhardt <m.burkhardt@kauschke.eu>
2023-09-04 11:11:43 +0200

    Added .gitignore file. Restructured AssemblyInfo and set version to 0.1.0.0
    Signed-off-by: Maximilian Burkhardt <m.burkhardt@kauschke.eu>

--- MultiSelectTreeViewEx/Properties/AssemblyInfo.cs
+++ MultiSelectTreeViewEx/Properties/AssemblyInfo.cs
@@ -1,11 +1,18 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("MultiSelectTreeViewEx")]
+[assembly: AssemblyDescription("Library for a multi select TreeView and virtualization")]
+[assembly: AssemblyCompany("Kauschke Engineering Systems GmbH")]
+[assembly: AssemblyProduct("MultiSelectTreeViewEx")]
+[assembly: AssemblyCopyright("Copyright © 2023 - Kauschke Engineering Systems GmbH")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 
 // General Information about an assembly is controlled through the following 
@@ -17,20 +24,30 @@ using System.Windows;
 + "9fcde9303efff3ab483057078d89598ea542edea150340e0f842a99aa7585cec36ded398f9121e"
 + "b0c19b6ef7791ac7b53940fce9bd04ae225097b69e7354da663168ddc415216903ac68a5d2698b"
 + "0dd9edc1")]
-[assembly: AssemblyTitle("MultiSelectTreeViewEx")]
-[assembly: AssemblyDescription("Library for a multi select TreeView")]
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCompany("-")]
-[assembly: AssemblyProduct("MultiSelectTreeViewEx")]
-[assembly: AssemblyCopyright("Copyright © Kauschke Engineering Services GmbH 2023")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
+
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.SourceAssembly, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page, 
-    // or application resource dictionaries)
+                                               //(used if a resource is not found in the page, 
+                                               // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page, 
-    // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
 )]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
